5d18a41570ed7fc633eb8b9757ab6d3d0427f6a6
Samyak Jain <34437877+KaymasJain@users.noreply.github.com>
2020-04-13 03:35:09 +1000

    Compound resolver done

--- src/protocol/compound.js
+++ src/protocol/compound.js
@@ -7,6 +7,8 @@ module.exports = class Compound {
     this.address = _dsa.address;
     this.tokens = _dsa.tokens;
     this.web3 = _dsa.web3;
+    this.instance = _dsa.instance;
+    this.dsa = _dsa;
   }
 
   /**
@@ -22,82 +24,87 @@ module.exports = class Compound {
     }
   }
 
+  getCtokens() {
+    var _tokens = this.tokens.info;
+    var _ctokens = {};
+    Object.keys(_tokens).forEach((_token, i) => {
+      if (_tokens[_token].type == "ctoken") _ctokens[_token] = _tokens[_token];
+    });
+    return _ctokens;
+  }
+
+  getCtokensAddresses(_ctokens) {
+    if (!_ctokens) _ctokens = getCtokens();
+    var _cAddresses = [];
+    Object.keys(_ctokens).forEach((_key, i) => {
+      _cAddresses.push(_ctokens[_key].address);
+    });
+    return _cAddresses;
+  }
+
   /**
    * get properly formatted compound position details
    * @param {string} address the owner address
    * @param {string} cTokens the cToken address
    */
-  getPosition(address, cTokens) {
-    var _c = new this.web3.eth.Contract(
-      this.ABI.read["compound"],
-      this.address.read["compound"]
-    );
+  getPosition(address) {
+    var _address;
+    !address ? (_address = this.instance.address) : (_address = address);
+    var _ctokens = this.getCtokens();
+    var _cAddresses = this.getCtokensAddresses(_ctokens);
+    console.log(_address);
+    console.log(_cAddresses);
+    var _obj = {
+      protocol: "compound",
+      method: "getPosition",
+      args: [_address, _cAddresses],
+    };
     return new Promise(async (resolve, reject) => {
-      let compoundRawData = await _c.methods
-        .getPosition(address, cTokens)
-        .call()
+      await this.dsa
+        .read(_obj)
         .then((res) => {
-          return res;
+          var _position = {};
+          var _totalSupplyInEth = 0;
+          var _totalBorrowInEth = 0;
+          var _maxBorrowLimitInEth = 0;
+          Object.keys(_ctokens).forEach((key, i) => {
+            var _res = res[i];
+            var _decimals = this.tokens.info[_ctokens[key].root].decimals;
+            _position[key] = {};
+            var _priceInEth = _res[0] / 10 ** (18 + (18 - _decimals));
+            _position[key].priceInEth = _priceInEth;
+            var _exchangeRate = _res[1] / 1e18;
+            var _supply = (_res[2] * _exchangeRate) / 10 ** _decimals;
+            _position[key].supply = _supply;
+            _totalSupplyInEth += _supply * _priceInEth;
+            _maxBorrowLimitInEth +=
+              _supply * _priceInEth * _ctokens[key].factor;
+            var _borrow = _res[3] / 10 ** _decimals;
+            _position[key].borrow = _borrow;
+            _totalBorrowInEth += _borrow * _priceInEth;
+            var _supplyRate = (_res[4] * 2102400) / 1e18;
+            _position[key].supplyRate = _supplyRate * 100; // Multiply with 100 to make it in percent
+            var _supplyYield = (1 + _supplyRate / 365) ** 365 - 1;
+            _position[key].supplyYield = _supplyYield * 100; // Multiply with 100 to make it in percent
+            var _borrowRate = (_res[5] * 2102400) / 1e18;
+            _position[key].borrowRate = _borrowRate * 100; // Multiply with 100 to make it in percent
+            var _borrowYield = (1 + _borrowRate / 365) ** 365 - 1;
+            _position[key].borrowYield = _borrowYield * 100; // Multiply with 100 to make it in percent
+          });
+          _position.totalSupplyInEth = _totalSupplyInEth;
+          _position.totalBorrowInEth = _totalBorrowInEth;
+          _position.maxBorrowLimitInEth = _maxBorrowLimitInEth;
+          _position.remainingBorrowInEth =
+            _maxBorrowLimitInEth - _totalBorrowInEth;
+          var _status = _totalBorrowInEth / _totalSupplyInEth;
+          _position.status = _status;
+          var _liquidationPoint = _maxBorrowLimitInEth / _totalSupplyInEth;
+          _position.liquidation = _liquidationPoint;
+          resolve(_position);
         })
         .catch((err) => {
           reject(err);
         });
-
-      var compoundDataRawArr = [];
-      compoundRawData.forEach((token) => {
-        compoundDataRawArr.push(...token);
-      });
-
-      let addrDecimal = {};
-      let addrSymb = {};
-      Object.values(this.tokens.info).forEach((token) => {
-        if (token.compound) {
-          let _token = this.ctokenMap[token.symbol.toLowerCase()];
-          addrDecimal[token.address] = that.token[_token].decimals;
-          addrSymb[token.address] = that.token[_token].symbol;
-        }
-      });
-
-      let compoundCalculatedData = {};
-      cTokens.forEach(function (key, i) {
-        let tokenData = {
-          oraclePrice: 0,
-          Token_symbol: addrSymb[key],
-          exchangeRate: 0,
-          supplyBalance: 0,
-          borrowBalance: 0,
-          supplyRate: 0,
-          borrowRate: 0,
-        };
-        var fromElement = i * 6;
-        var tokensPriceOracle =
-          compoundDataRawArr[fromElement] /
-          10 ** (18 + (18 - addrDecimal[key]));
-        tokenData.oraclePrice = tokensPriceOracle;
-        var compoundExchangeRate = compoundDataRawArr[fromElement + 1] / 1e18;
-        var balanceOfCToken = compoundDataRawArr[fromElement + 2];
-        var compoundTokenBal = { borrow: 0, supply: 0 };
-        compoundTokenBal.supply =
-          (balanceOfCToken * compoundExchangeRate) / 10 ** addrDecimal[key];
-        compoundTokenBal.supply * tokensPriceOracle;
-        var borrowBal =
-          compoundDataRawArr[fromElement + 3] / 10 ** addrDecimal[key];
-        compoundTokenBal.borrow = borrowBal;
-        var compoundSupplyRate =
-          (compoundDataRawArr[fromElement + 4] * 2102400) / 1e18;
-        compoundSupplyRate = ((1 + compoundSupplyRate / 365) ** 365 - 1) * 100;
-        var compoundBorrowRate =
-          (compoundDataRawArr[fromElement + 5] * 2102400) / 1e18;
-        compoundBorrowRate = ((1 + compoundBorrowRate / 365) ** 365 - 1) * 100;
-        tokenData.exchangeRate = compoundExchangeRate;
-        tokenData.supplyBalance = compoundTokenBal.supply;
-        tokenData.borrowBalance = compoundTokenBal.borrow;
-        tokenData.supplyRate = compoundSupplyRate;
-        tokenData.borrowRate = compoundBorrowRate;
-        compoundCalculatedData[key] = tokenData;
-      });
-      compoundCalculatedData.authority = address;
-      resolve(compoundCalculatedData);
     });
   }
 };
