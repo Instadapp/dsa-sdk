c6425462a7a0c073c81e0cf0901bac6ff39d9aed
Sowmay Jain <sowmayjn@gmail.com>
2020-04-14 11:59:01 +1000

    minor edits

--- src/token.js
+++ src/token.js
@@ -1,14 +1,17 @@
+/**
+ * generic ERC20 token methods
+ */
 module.exports = class Token {
-  /**
-   * @param {Object} _dsa the dsa instance to access data stores
-   */
-  constructor(_dsa) {
-    this.ABI = _dsa.ABI;
-    this.tokens = _dsa.tokens;
-    this.helpers = _dsa.helpers;
-    this.internal = _dsa.internal;
-    this.web3 = _dsa.web3;
-  }
+    /**
+     * @param {Object} _dsa the dsa instance to access data stores
+     */
+    constructor(_dsa) {
+        this.ABI = _dsa.ABI;
+        this.tokens = _dsa.tokens;
+        this.helpers = _dsa.helpers;
+        this.internal = _dsa.internal;
+        this.web3 = _dsa.web3;
+    }
 
     /**
      * Transfer
@@ -18,25 +21,29 @@ module.exports = class Token {
      * @param {address} _d.from (optional)
      * @param {number|string} _d.gasPrice (optional)
      * @param {number|string} _d.gas (optional)
-    */
+     */
     async transfer(_d) {
         let _addr = await this.internal.getAddress();
         let web3 = this.web3;
-        if(!_d.token || Object.keys(this.tokens.info).indexOf(_d.token.toLowerCase()) == -1) throw new Error("'token' symbol not found.")
-        if(!_d.to) throw new Error("'to' address is not defined")
-        if(!_d.amount) throw new Error("'amount' is not defined")
+        if (!_d.token || Object.keys(this.tokens.info).indexOf(_d.token.toLowerCase()) == -1) throw new Error("'token' symbol not found.")
+        if (!_d.to) throw new Error("'to' address is not defined")
+        if (!_d.amount) throw new Error("'amount' is not defined")
         var token = this.tokens.info[_d.token.toLowerCase()];
         if (!_d.from) _d.from = _addr;
-        _d.amount = this.helpers.bigNumInString((_d.amount*10**token.decimals).toFixed(0));
+        _d.amount = this.helpers.bigNumInString((_d.amount * 10 ** token.decimals).toFixed(0));
         if (_d.token.toLowerCase() == "eth") {
             return new Promise((resolve, reject) => {
-                return web3.eth.sendTransaction({from: _d.from, to: _d.to, value: _d.amount})
-                .on("transactionHash", (txHash) => {
-                    resolve(txHash);
-                })
-                .on("error", (err) => {
-                    reject(err);
-                });
+                return web3.eth.sendTransaction({
+                        from: _d.from,
+                        to: _d.to,
+                        value: _d.amount
+                    })
+                    .on("transactionHash", (txHash) => {
+                        resolve(txHash);
+                    })
+                    .on("error", (err) => {
+                        reject(err);
+                    });
             });
         } else {
             var _c = await new web3.eth.Contract(this.ABI.basic.erc20, token.address);
@@ -45,11 +52,11 @@ module.exports = class Token {
                     .transfer(_d.to, _d.amount)
                     .send(_d)
                     .on("transactionHash", (txHash) => {
-                    resolve(txHash);
+                        resolve(txHash);
                     })
                     .on("error", (err) => {
-                    reject(err);
-                });
+                        reject(err);
+                    });
             });
         }
     }
@@ -62,19 +69,21 @@ module.exports = class Token {
      * @param {address} _d.from (optional)
      * @param {number|string} _d.gasPrice (optional)
      * @param {number|string} _d.gas (optional)
-    */
+     */
     async approve(_d) {
         let _addr = await this.internal.getAddress();
         let web3 = this.web3;
-        if(!_d.token || Object.keys(this.tokens.info).indexOf(_d.token.toLowerCase()) == -1) throw new Error("'token' symbol not found.")
-        if(!_d.to) throw new Error("'to' address is not defined")
-        if(!_d.amount) throw new Error("'amount' is not defined")
+        if (!_d.token || Object.keys(this.tokens.info).indexOf(_d.token.toLowerCase()) == -1) throw new Error("'token' symbol not found.")
+        if (!_d.to) throw new Error("'to' address is not defined")
+        if (!_d.amount) throw new Error("'amount' is not defined")
         var token = this.tokens.info[_d.token.toLowerCase()];
         if (!_d.from) _d.from = _addr;
-        _d.amount = this.helpers.bigNumInString((_d.amount*10**token.decimals).toFixed(0));
+        _d.amount = this.helpers.bigNumInString((_d.amount * 10 ** token.decimals).toFixed(0));
 
         if (_d.token.toLowerCase() == "eth") {
-            return new Promise((resolve, reject) => {resolve("ETH does not require approve.")})
+            return new Promise((resolve, reject) => {
+                resolve("ETH does not require approve.")
+            })
         } else {
             var _c = await new web3.eth.Contract(this.ABI.basic.erc20, token.address);
             return new Promise((resolve, reject) => {
@@ -82,14 +91,14 @@ module.exports = class Token {
                     .approve(_d.to, _d.amount)
                     .send(_d)
                     .on("transactionHash", (txHash) => {
-                    resolve(txHash);
+                        resolve(txHash);
                     })
                     .on("error", (err) => {
-                    reject(err);
-                });
+                        reject(err);
+                    });
             });
         }
-        
+
     }
 
     /**
@@ -97,16 +106,18 @@ module.exports = class Token {
      * @param {symbol} _d.token
      * @param {address} _d.to
      * @param {address} _d.from (optional)
-    */
+     */
     async getAllowance(_d) {
         let _addr = await this.internal.getAddress();
         let web3 = this.web3;
-        if(!_d.token || Object.keys(this.tokens.info).indexOf(_d.token.toLowerCase()) == -1) throw new Error("'token' symbol not found.")
-        if(!_d.to) throw new Error("'to' address is not defined")
+        if (!_d.token || Object.keys(this.tokens.info).indexOf(_d.token.toLowerCase()) == -1) throw new Error("'token' symbol not found.")
+        if (!_d.to) throw new Error("'to' address is not defined")
         if (!_d.from) _d.from = _addr;
 
         if (_d.token.toLowerCase() == "eth") {
-            return new Promise((resolve, reject) => {resolve("ETH does not have allowance.")})
+            return new Promise((resolve, reject) => {
+                resolve("ETH does not have allowance.")
+            })
         } else {
             var _c = await new web3.eth.Contract(
                 this.ABI.basic.erc20,
@@ -114,13 +125,13 @@ module.exports = class Token {
             );
             return new Promise(function (resolve, reject) {
                 return _c.methods.allowance(_d.from, _d.to)
-                .call()
-                .then((res) => {
-                    resolve(res);
-                })
-                .catch((err) => {
-                    reject(err);
-                });
+                    .call()
+                    .then((res) => {
+                        resolve(res);
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    });
             });
         }
     }
