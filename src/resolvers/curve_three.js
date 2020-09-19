60fe5e47fcf1b3cacf1f2d2382d27b4c52454c8a
Thrilok Kumar <thrilok2000@gmail.com>
2020-09-19 12:42:58 +0530

    updated v1.4.39 and published

--- src/resolvers/curve_three.js
+++ src/resolvers/curve_three.js
@@ -1,55 +1,54 @@
 module.exports = class CurveThree {
-    /**
-     * @param {Object} _dsa the dsa instance to access data stores
-     */
-    constructor(_dsa) {
-      this.ABI = _dsa.ABI;
-      this.address = _dsa.address;
-      this.tokens = _dsa.tokens;
-      this.web3 = _dsa.web3;
-      this.instance = _dsa.instance;
-      this.math = _dsa.math;
-      this.dsa = _dsa;
-    }
+  /**
+   * @param {Object} _dsa the dsa instance to access data stores
+   */
+  constructor(_dsa) {
+    this.ABI = _dsa.ABI;
+    this.address = _dsa.address;
+    this.tokens = _dsa.tokens;
+    this.web3 = _dsa.web3;
+    this.instance = _dsa.instance;
+    this.math = _dsa.math;
+    this.dsa = _dsa;
+  }
 
-    /**
-     * returns buy/dest amount and unit Amount
-     * @param buyToken buy token symbol
-     * @param sellToken sell token symbol
-     * @param sellAmt sell token amount in decimal
-     * @param slippage slippage of trade
-     */
-    async getBuyAmount(buyToken, sellToken, sellAmt, slippage) {
-      let _slippage = !slippage ? 10 ** 16 : slippage * 10 ** 16;
-      _slippage = String(this.math.bigNumInString(_slippage));
-  
-      var _obj = {
-        protocol: "curve_three",
-        method: "getBuyAmount",
-        args: [
-          this.tokens.info[buyToken.toLowerCase()].address,
-          this.tokens.info[sellToken.toLowerCase()].address,
-          this.tokens.fromDecimal(sellAmt, sellToken),
-          this.math.bigNumInString(_slippage),
-        ],
-      };
-  
-      return new Promise((resolve, reject) => {
-        return this.dsa
-          .read(_obj)
-          .then((res) => {
-            var _res = {
-              buyAmt: this.tokens.toDecimal(res[0], buyToken),
-              buyAmtRaw: res[0],
-              virtualPrice: res[2] / 10 ** 18,
-              unitAmt: res[1],
-            };
-            resolve(_res);
-          })
-          .catch((err) => {
-            reject(err);
-          });
-      });
-    }
-  };
-  
+  /**
+   * returns buy/dest amount and unit Amount
+   * @param buyToken buy token symbol
+   * @param sellToken sell token symbol
+   * @param sellAmt sell token amount in decimal
+   * @param slippage slippage of trade
+   */
+  async getBuyAmount(buyToken, sellToken, sellAmt, slippage) {
+    let _slippage = !slippage ? 10 ** 16 : slippage * 10 ** 16;
+    _slippage = String(this.math.bigNumInString(_slippage));
+
+    var _obj = {
+      protocol: "curve_three",
+      method: "getBuyAmount",
+      args: [
+        this.tokens.info[buyToken.toLowerCase()].address,
+        this.tokens.info[sellToken.toLowerCase()].address,
+        this.tokens.fromDecimal(sellAmt, sellToken),
+        this.math.bigNumInString(_slippage),
+      ],
+    };
+
+    return new Promise((resolve, reject) => {
+      return this.dsa
+        .read(_obj)
+        .then((res) => {
+          var _res = {
+            buyAmt: this.tokens.toDecimal(res[0], buyToken),
+            buyAmtRaw: res[0],
+            virtualPrice: res[2] / 10 ** 18,
+            unitAmt: res[1],
+          };
+          resolve(_res);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+};
