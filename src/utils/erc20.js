fb05eabcf688df578c48b3d532473f6146f18aa4
Thrilok Kumar <thrilok2000@gmail.com>
2021-02-06 22:15:37 +0530

    Added txObj for transfer and approve

--- src/utils/erc20.js
+++ src/utils/erc20.js
@@ -78,6 +78,62 @@ module.exports = class Erc20 extends Erc20Resolver {
   }
 
   /**
+   * Transfer Tx object
+   * @param {symbol|address} _d.token
+   * @param {number|string} _d.amount
+   * @param {address} _d.to (optional) - default is DSA Address
+   * @param {address} _d.from (optional) - default is User's Address
+   * @param {number|string} _d.gasPrice (optional)
+   * @param {number|string} _d.gas (optional)
+   */
+  async transferTxObj(_d) {
+    let _addr = await this.internal.getAddress();
+    let _dsa = !this.dsa ? this : this.dsa;
+    if (!_d.token) throw new Error("'token' is not defined.");
+    if (!_d.to) {
+      _d.to = _dsa.instance.address;
+      if (_d.to == _dsa.address.genesis)
+        throw new Error("'to' is not defined and instance is not set.");
+    }
+    if (!_d.amount) throw new Error("'amount' is not defined");
+    if (!_d.from) _d.from = _addr;
+
+    let txObj;
+    if (
+      _d.token.toLowerCase() == "eth" ||
+      _d.token.toLowerCase() == this.tokens.info.eth.address
+    ) {
+      if (_d.amount == "-1" || _d.amount == _dsa.maxValue)
+        throw new Error("ETH amount value cannot be passed as '-1'.");
+      _d.value = _d.amount;
+      _d.callData = "0x";
+      txObj = await this.internal.getTxObj(_d);
+    } else {
+      _d.toAddr = _d.to;
+      _d.to = this.internal.filterAddress(_d.token);
+      var _c = await new this.web3.eth.Contract(
+        this.ABI.basic.erc20,
+        this.internal.filterAddress(_d.token)
+      );
+      if (_d.amount == "-1" || _d.amount == _dsa.maxValue) {
+        await _c.methods
+          .balanceOf(_d.from)
+          .call()
+          .then((bal) => (_d.amount = bal))
+          .catch((err) => {
+            throw new Error(`Error while getting token balance: ${err}`);
+          });
+      }
+      _d.callData = _c.methods
+        .transfer(_d.toAddr, this.math.bigNumInString(_d.amount))
+        .encodeABI();
+      txObj = await this.internal.getTxObj(_d);
+    }
+
+    return txObj;
+  }
+
+  /**
    * Approve Token
    * @param {symbol|address} _d.token
    * @param {number|string} _d.amount
@@ -118,6 +174,44 @@ module.exports = class Erc20 extends Erc20Resolver {
         .then((tx) => resolve(tx))
         .catch((err) => reject(err));
     });
+  }
+
+  /**
+   * Approve Token Tx Obj
+   * @param {symbol|address} _d.token
+   * @param {number|string} _d.amount
+   * @param {address} _d.to
+   * @param {address} _d.from (optional)
+   * @param {number|string} _d.gasPrice (optional)
+   * @param {number|string} _d.gas (optional)
+   */
+  async approveTxObj(_d) {
+    let _addr = await this.internal.getAddress();
+    let _dsa = this.dsa;
+    if (!_d.token) throw new Error("'token' is not defined.");
+    if (!_d.to) throw new Error("'to' address is not defined");
+    if (!_d.amount) throw new Error("'amount' is not defined");
+    if (!_d.from) _d.from = _addr;
+
+    let txObj;
+    if (
+      _d.token.toLowerCase() == "eth" ||
+      _d.token.toLowerCase() == this.tokens.info.eth.address
+    ) {
+      return new Promise((resolve, reject) => {
+        reject("ETH does not require approve.");
+      });
+    } else {
+      _d.toAddr = _d.to;
+      _d.to = this.internal.filterAddress(_d.token);
+      var _c = await new this.web3.eth.Contract(
+        this.ABI.basic.erc20,
+        this.internal.filterAddress(_d.token)
+      );
+      _d.callData = _c.methods.approve(_d.toAddr, _d.amount).encodeABI();
+      txObj = await this.internal.getTxObj(_d);
+    }
+    return txObj;
   }
 
   /**
